f771957875616ab4f3fd300a74b4d3bff2003c0d
Skellig325 <davidposullivan1@gmail.com>
2024-10-16 23:09:35 +0100

    Create server.ts
    Server init

---
+++ Server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+app.use(express.json());    
+app.use(express.urlencoded({extended: false})); 
+
+const port: number = process.env.PORT ? Number(process.env.PORT) : 8000;
+app.listen(port, () => {
+    console.log('Server Started On Port 8000');
+});
+
