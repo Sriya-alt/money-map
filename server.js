4c88a237ab78ccfe58e3225afbb6b072186bab05
dxrnok <konrad.sokal@gmail.com>
2024-10-16 23:43:34 +0100

    update

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());    
-app.use(express.urlencoded({extended: false})); 
-
-app.listen(8000, () => {
-    console.log('Server Started On Port 8000');
-});
