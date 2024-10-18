da0d5e382ad6679b33ac1177fc1437dde5ef0d0d
dxrnok <konrad.sokal@gmail.com>
2024-10-18 22:31:36 +0100

    update

--- Server/server.ts
+++ Server/server.ts
@@ -5,18 +5,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const port: number = process.env.PORT ? Number(process.env.PORT) : 8000;
-<<<<<<< HEAD
 app.listen(port, () => {
     console.log(`Server Started On Port ${port}`);
 });
-=======
-
-app.get('/', (req, res) => {
-    res.send('Hello, welcome to the backend!');
-});
-
-app.listen(port, () => {
-    console.log(`Server Started On Port ${port}`);
-});
-
->>>>>>> e5f8ddd5d5b965a33546800d52248c71ca9ff09a
