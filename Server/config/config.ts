f9673032a2856a16c4b155aa742f5834bffe7747
dxrnok <konrad.sokal@gmail.com>
2024-10-21 21:36:52 +0100

    added database connection

--- Server/config/config.ts
+++ Server/config/config.ts
@@ -9,9 +9,14 @@ WE NEED TO CREATE A CONFIG FILE TO STORE ALL THE DATABASE CONFIGURATIONS.
 SUPABASE_API=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViYXJlbmJnZWZ5emljb29scGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxOTgwNTMsImV4cCI6MjA0NDc3NDA1M30.77976gFHWn14kP8IV0A0aKEdCrMlhNNEp5nSbcbrZjw 
  */
 import { createClient } from '@supabase/supabase-js'
+import * as dotenv from 'dotenv';
+
+// Load the .env file and make variables available in process.env
+dotenv.config();
+
 // Ensure the environment variables are defined
-const supabaseUrl = 'https://ebarenbgefyzicoolpbg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViYXJlbmJnZWZ5emljb29scGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxOTgwNTMsImV4cCI6MjA0NDc3NDA1M30.77976gFHWn14kP8IV0A0aKEdCrMlhNNEp5nSbcbrZjw ';
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_API || '';
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase configuration. Please check your .env file.');
