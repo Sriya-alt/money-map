510b00e86bf0e48e6ff9082ae1ea35fdc9d0b35a
dxrnok <konrad.sokal@gmail.com>
2024-10-21 21:53:59 +0100

    connection update

--- Server/config/config.ts
+++ Server/config/config.ts
@@ -5,9 +5,7 @@ THIS IS FOR DATABASE CONFIGURATION AND CONNECTION SUCH AS URL, PORT, DATABASE NA
 
 WE NEED TO CREATE A CONFIG FILE TO STORE ALL THE DATABASE CONFIGURATIONS.
 */ 
-/* SUPABASE_URL=https://ebarenbgefyzicoolpbg.supabase.co
-SUPABASE_API=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViYXJlbmJnZWZ5emljb29scGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxOTgwNTMsImV4cCI6MjA0NDc3NDA1M30.77976gFHWn14kP8IV0A0aKEdCrMlhNNEp5nSbcbrZjw 
- */
+
 import { createClient } from '@supabase/supabase-js'
 import * as dotenv from 'dotenv';
 
