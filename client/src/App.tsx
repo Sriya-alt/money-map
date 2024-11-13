73d9b47a6c2f1e1a99ff306d69447d9dbf4418bc
dxrnok <konrad.sokal@gmail.com>
2024-11-13 11:44:08 +0000

    updating

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Onboarding from './pages/Onboarding';
-import AuthSelection from './pages/AuthSelection';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import DebtQuestion from './pages/DebtQuestion';
-import SubscriptionQuestion from './pages/SubscriptionQuestion';
-import SavingsGoalQuestion from './pages/SavingsGoalQuestion';
-import FinanceFeelQuestion from './pages/FinanceFeelQuestion';
-import SpendingQuestion from   './pages/SpendingQuestion';
-import ExpenditureQuestion from './pages/ExpenditureQuestion';
-import BudgetAllocation from './pages/BudgetAllocation'; 
-import SummaryPage from './pages/SummaryPage';
+import logo from './assets/logo.svg';
+import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Onboarding />} />
-        <Route path="/auth-selection" element={<AuthSelection />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/questions/debt" element={<DebtQuestion />} />
-        <Route path="/questions/subscriptions" element={<SubscriptionQuestion />} />
-        <Route path="/questions/savings-goals" element={<SavingsGoalQuestion />} />
-        <Route path="/questions/finance-feel" element={<FinanceFeelQuestion />} />
-        <Route path="/questions/spendings" element={<SpendingQuestion />} />
-        <Route path="/questions/expenditures" element={<ExpenditureQuestion />} />
-        <Route path="/questions/budget-allocations" element={<BudgetAllocation />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/summary" element={<SummaryPage />} />
-        <Route path="*" element={<Onboarding />} />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { QuestionnaireProvider } from './context/QuestionnaireContext';
-import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <QuestionnaireProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QuestionnaireProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
