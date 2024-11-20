6b6fe47498bd9b9de092b8ec721971b0fd36177f
Konrad <konrad.sokal.2023@mumail.ie>
2024-11-20 09:59:03 +0000

    update NavBar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,10 @@
-
+export{};
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
 
-const Navbar: React.FC = () => {
-  return (
-    <nav className="Navbar">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/budget">Budget</Link></li>
-        <li><Link to="/transactions">Transactions</Link></li>
-        <li><Link to="/account">Account</Link></li>
-      </ul>
-    </nav>
-  );
-}
+const Navbar: React.FC = () => (
+  <nav className="navbar">
+    <h1>Money Map</h1>
+  </nav>
+);
 
 export default Navbar;
