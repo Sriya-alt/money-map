0fb360e83caa25ada3fe213ea14885ccf800dc94
Konrad <konrad.sokal.2023@mumail.ie>
2024-11-22 22:49:12 +0000

    fully functional route

--- Server/routes/login.ts
+++ Server/routes/login.ts
@@ -1,71 +1,35 @@
 import express from 'express';
 import supabase from "../config/config";
-import { validate } from '../controllers/hashing';
-import { searchUser } from '../controllers/db';
 
-const router = express.Router();
+const router = express();
 //const {users,...} = require('../db');
 
-router.post('/', async (req, res) => {
-    const { email, password} = req.body;
+router.get('/', (req, res) => {
+    res.render('login');
+});
+
+router.post('/', (req, res) => {
     try{
-        if(!email || !password){
-            res.status(400).json({ success: false, error: 'All fields are required.' });
-            return;
-        }
+        if(req.body.button === 'login'){
+            //include salt hash///
+            const email = req.body.email;
+            const passwd = req.body.passowrd;
 
-        const userData = await searchUser(email);
-        var hashP = false;
+            const script = `
+                <script>
+                    alert('Login Successfull!');
+                    window.location.href = '/';
+                </script>
+            `;
 
-        if(userData.length > 0 && userData != null){
-            const pass = userData[0];
-            hashP = await validate(password, String(pass.password));
-        }
-
-        if(hashP){
-            console.log('User Found successfully:', userData);
-            res.status(201).json({success: true, message: 'User Found Successfully.'});
-            return;
-        }else{
-            res.status(500).json({success: false, error: 'Wrong Email or Password.' });
-            return;
+            //Successful status and exec script
+            res.status(200).send(script);
         }
     }catch(error){
-        console.error('Error During Login:', error);
-        res.status(400).json({success: false, error: 'Server Error'});
-        return;
+        console.error('Error Registering:', error);
+        res.status(400).send('Error Registering');
     }
 });
-
-/*
-
-    router.post('/', async (req, res) => {
-    const { email, password } = req.body;
-    console.log("Received data:", { email, password });
-    try {
-        if (!email || !password) {
-            return res.status(400).json({ error: 'All fields are required.' });
-        }
-        const userData = await searchUser(email);
-        
-        var hashP = false;
-        if (userData != null && userData.length > 0) {
-            const user = userData[0];
-            hashP = await validate(password, String(user.password));
-            console.log(String(user.password));
-        }
-        if (hashP) {
-            console.log('User Found successfully:', userData);
-            return res.status(201).json({ message: 'User Found Successfully.' });
-        } else {
-            return res.status(500).json({ error: 'Failed to find user.' });
-        }
-    } catch (error) {
-        console.error('Error During Login:', error);
-        return res.status(400).json({ error: 'Server Error' });
-    }
-});
-*/
 
 //how to encode and decode the passwd so it can always be compared when needed for login.
 async function searchDB(email: string, passwd: string){
