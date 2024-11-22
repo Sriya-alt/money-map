8fb02ba6eb7f6df42f8da350a6f355ebd5d334df
Konrad <konrad.sokal.2023@mumail.ie>
2024-11-22 22:48:16 +0000

    Added More Functions

--- Server/controllers/db.ts
+++ Server/controllers/db.ts
@@ -1,15 +1,15 @@
 import supabase from "../config/config";
-import {hashEmail, hashPassword} from "../controllers/hashing";
+import {hashEmail, hashPassword} from "./hashing";
 
-export async function insertUser(email: string, psswd: string) {
+export async function insertUser(name: string, email: string, psswd: string, phoneNumber: string, dob: Date) {
     try {
-        const emailHash = await hashEmail(email);
+        const emailHash = await hashEmail(email.toLowerCase());
         const psswdHash = await hashPassword(psswd);
 
         const { data, error } = await supabase
             .from('users')
             .insert([
-            { email: emailHash, password: psswdHash}, 
+            {name: capitalizeWords(name), email: emailHash, password: psswdHash, phone: phoneNumber, dob: dob}, 
         ])
         .select();
         if(data){
@@ -26,14 +26,12 @@ export async function insertUser(email: string, psswd: string) {
 }
 
 //how to encode and decode the passwd so it can always be compared when needed for login.
-async function searchDB(email: string, passwd: string){
+export async function searchPassword(psswd: string): Promise<object[] | null>{
     try{
         const {data, error} = await supabase
             .from('users')
-            .insert([
-            {password: passwd, email: email}, 
-        ])
-        .select();
+            .select('*') // Adjust fields based on what you need
+            .eq('password', psswd);
 
         if(error){
             console.error('Credentials Incorrect: ', error.message);
@@ -44,4 +42,31 @@ async function searchDB(email: string, passwd: string){
         console.error('Unexpected error:', err);
         return null;
     }
+}
+//
+export async function searchUser(email: string) : Promise<{password: string;}[]>{
+    try{
+        const hash = hashEmail(email);
+        const {data, error} = await supabase
+            .from('users')
+            .select('*') // Adjust fields based on what you need
+            .eq('email', hash); // Filters records with a matching email
+
+        if(error){
+            console.error('Credentials Incorrect: ', error.message);
+            return [];
+        }
+        return data;
+    }catch (err){
+        console.error('Unexpected error:', err);
+        return [];
+    }
+}
+
+function capitalizeWords(str: string): string {
+    return str
+        .trim() // Remove leading and trailing spaces
+        .split(/\s+/) // Split by one or more spaces
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize first letter and make the rest lowercase
+        .join(' ');
 }
