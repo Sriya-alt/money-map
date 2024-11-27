d55679883c616b6dbf357d821c11e4ca9288e1cc
David <david.osullivan.2023@mumail.ie>
2024-11-27 22:26:29 +0000

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,56 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Budget from './pages/Budget';
-import Transactions from './pages/Transactions';
-import Account from './pages/Account';
+import { Route, Routes } from 'react-router-dom';
+
+// Importing pages
+import Onboarding from './pages/Onboarding';
+import AuthSelection from './pages/AuthSelection';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
-import { QuestionnaireProvider } from './context/QuestionnaireContext';
-import './App.css'; 
+import DebtQuestion from './pages/DebtQuestion';
+import SubscriptionQuestion from './pages/SubscriptionQuestion';
+import SavingsGoalQuestion from './pages/SavingsGoalQuestion';
+import FinanceFeelQuestion from './pages/FinanceFeelQuestion';
+import SpendingQuestion from './pages/SpendingQuestion';
+import ExpenditureQuestion from './pages/ExpenditureQuestion';
+import BudgetAllocation from './pages/BudgetAllocation';
+import SummaryPage from './pages/SummaryPage';
+import NotificationPage from './pages/NotificationPage'; // Added notification page
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <QuestionnaireProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/budget" element={<Budget />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </QuestionnaireProvider>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Onboarding />} />
+
+        {/* Authentication Routes */}
+        <Route path="/auth-selection" element={<AuthSelection />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Questionnaire Routes */}
+        <Route path="/questions/debt" element={<DebtQuestion />} />
+        <Route path="/questions/subscriptions" element={<SubscriptionQuestion />} />
+        <Route path="/questions/savings-goals" element={<SavingsGoalQuestion />} />
+        <Route path="/questions/finance-feel" element={<FinanceFeelQuestion />} />
+        <Route path="/questions/spendings" element={<SpendingQuestion />} />
+        <Route path="/questions/expenditures" element={<ExpenditureQuestion />} />
+        <Route path="/questions/budget-allocations" element={<BudgetAllocation />} />
+
+        {/* Main Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Notifications */}
+        <Route path="/notifications" element={<NotificationPage />} />
+
+        {/* Summary Page */}
+        <Route path="/summary" element={<SummaryPage />} />
+
+        {/* Fallback route */}
+        <Route path="*" element={<Onboarding />} />
+      </Routes>
+    </div>
   );
 }
 
